625df9d0029b417855d4224e76818d26a56e9f1a
njoromyke <mykenjoroge01@gmail.com>
2023-11-08 10:34:27 +0300

    IActionResult

---
+++ Controllers/ProductIActionController.cs
@@ -0,0 +1,29 @@
+﻿using AdvWorksAPI.EntityLayer;
+using AdvWorksAPI.RepositoryLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvWorksAPI.Controllers;
+
+
+[Route("api/[controller]")]
+[ApiController]
+    public class ProductIActionController : Controller
+    {
+
+    [HttpGet]
+        public IActionResult Get()
+        {
+            IActionResult ret;
+            List<Product> list;
+
+            list = new ProductRepository().Get();
+
+            //list.Clear();
+
+            ret = list?.Count > 0 ? StatusCode(StatusCodes.Status200OK, list) : StatusCode(StatusCodes.Status404NotFound, "No Product Found");
+
+            return ret;
+         
+        }
+    }
+
