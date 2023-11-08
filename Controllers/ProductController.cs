0a0f4c3cdef47dcd1523310e661166b7f4ed5867
njoromyke <mykenjoroge01@gmail.com>
2023-11-08 12:02:51 +0300

    multiple endpoints on same endpoint

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -4,52 +4,32 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AdvWorksAPI.Controllers;
 
-
 [Route("api/[controller]")]
 [ApiController]
 public class ProductController : Controller
 {
-
     [HttpGet]
+    [Route("")]
+    [Route("GetAll")]
+    [Route("GetAllProducts")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public ActionResult<IEnumerable<Product>> Get()
     {
         List<Product> list = new ProductRepository().Get();
 
-        return list?.Count > 0 ? StatusCode(StatusCodes.Status200OK, list) : StatusCode(StatusCodes.Status404NotFound, "No Product Found");
-
-
-    }
-
-
-
-    [HttpGet]
-    [Route("GetAll")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public ActionResult<IEnumerable<Product>> GetAll()
-    {
-        List<Product> list = new ProductRepository().Get();
-
-        return list?.Count > 0 ? StatusCode(StatusCodes.Status200OK, list) : StatusCode(StatusCodes.Status404NotFound, "No Product Found");
-
-
+        return list?.Count > 0
+            ? StatusCode(StatusCodes.Status200OK, list)
+            : StatusCode(StatusCodes.Status404NotFound, "No Product Found");
     }
 
     [HttpGet("{id}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-
     public ActionResult<Product?> Get(int id)
     {
-
         var entity = new ProductRepository().Get(id);
 
-
         return entity != null ? Ok(entity) : NotFound($"Cannot find product with id {id}");
-
-
     }
-
 }
