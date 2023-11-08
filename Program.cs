c0542391728e6f09de785a8b69bace4729743954
njoromyke <mykenjoroge01@gmail.com>
2023-11-08 14:57:35 +0300

    Singleton Lifetime in dependency injection

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 
-builder.Services.AddScoped<AdvWorksApiDefaults, AdvWorksApiDefaults>();
+//builder.Services.AddScoped<AdvWorksApiDefaults, AdvWorksApiDefaults>();
+builder.Services.AddSingleton<AdvWorksApiDefaults, AdvWorksApiDefaults>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
