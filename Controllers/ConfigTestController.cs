bd0ae0fa4f53d3dea62991eea04bbacfaf048a32
njoromyke <mykenjoroge01@gmail.com>
2023-11-13 17:25:35 +0300

    Reading confi settings in a class

---
+++ Controllers/ConfigTestController.cs
@@ -0,0 +1,24 @@
+﻿using AdvWorksAPI.BaseController;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvWorksAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ConfigTestController : ControllerBaseApi
+{
+    private readonly IConfiguration _config;
+
+    public ConfigTestController(ILogger<ConfigTestController> logger, IConfiguration config)
+        : base(logger)
+    {
+        _config = config;
+    }
+
+    [HttpGet]
+    [Route("IConfigurationTest")]
+    public string ConfigurationTest()
+    {
+        return _config["AdvWorksAPI:InfoMessageDefault"] ?? string.Empty;
+    }
+}
